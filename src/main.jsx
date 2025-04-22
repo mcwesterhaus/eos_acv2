7a23fc97b67065db35932047d427cc231a394ce9
mcwesterhaus <matt.w@heartlandsoilservices.com>
2025-04-22 09:27:42 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EOSFlowchartTool from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EOSFlowchartTool />
+  </React.StrictMode>
+);
